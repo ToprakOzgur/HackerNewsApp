c99ca4829d58155c143f48cce38168ac275c4b96
ToprakOzgur <zgrtprk@gmail.com>
2019-03-14 17:44:38 +0300

    repository class added

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -8,7 +8,7 @@ import '../models/item_model.dart';
 class NewsDbProvider {
   Database db;
 
-  init() async {
+  void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, "items.db");
 
@@ -38,7 +38,7 @@ class NewsDbProvider {
     );
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final maps = await db.query(
       "items",
       columns: null,
@@ -50,5 +50,9 @@ class NewsDbProvider {
       return ItemModel.fromDb(maps.first);
     }
     return null;
+  }
+
+  Future<int> addItem(ItemModel item) {
+    return db.insert("Items", item.toMap());
   }
 }
